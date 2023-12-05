aeb628ae4a732abf58d1c2e6f8efd7c5e2816cbf
koladetyk <emmaerinle@gmail.com>
2023-12-05 06:20:03 +0100

    first commit

---
+++ truffle.js
@@ -0,0 +1,19 @@
+var HDWalletProvider = require("truffle-hdwallet-provider");
+var mnemonic = "glow run scrap lumber income onion betray boring odor category light junk";
+
+module.exports = {
+  networks: {
+    development: {
+      provider: function() {
+        return new HDWalletProvider(mnemonic, "http://127.0.0.1:7545/", 0, 50);
+      },
+      network_id: '*',
+      gas: 6721975
+    }
+  },
+  compilers: {
+    solc: {
+      version: "^0.4.24"
+    }
+  }
+};
